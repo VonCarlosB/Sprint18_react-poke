389d6c52e1e511029e2eb667e57251bb2659c20d
CarlosBF <cborrellf@outlook.es>
2025-06-26 21:49:09 +0200

    Changes made during live review

--- src/App.jsx
+++ src/App.jsx
@@ -3,54 +3,54 @@ import './App.css';
 
 function App () {
   const [name, setName] = useState('')
-  const [pokemon, setPokemon] = useState({})
-
-  const pokemonContainer = () => {
-    if(pokemon.id != undefined){
-      return(
-      <div>
-        <h2><b>{pokemon.id}.</b> {pokemon.name}</h2>
-        <img src={pokemon.sprites.front_default} />
-      </div>
-      )
-    }else{
-      return(
-        <div>
-          <h2>{pokemon.name}</h2>
-        </div>
-      )
-    }
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    getPokemon(name)
-  }
+  const [pokemon, setPokemon] = useState(null)
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   const getPokemon = async (pokemonName) => {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     try {
+      setIsLoading(true)
       if(!res.ok){
         throw new Error ('No se ha podido acceder a la API')
       }
       const data = await res.json()
-      setPokemon(data)
-    } catch (error) {
-      setPokemon({name:'Enter your pokemon'})
+      data.results ? setPokemon(null) : setPokemon(data)
+    } catch (err) {
+      setError(err.message)
+      console.error(error)
+    } finally{
+      setIsLoading(false)
     }
   }
 
   useEffect(() => {
-    getPokemon(name)
+    setError(null)
+    setPokemon(null)
+
+    const delay = setTimeout(() => {
+      getPokemon(name)
+    }, 100)
+
+    return () => clearTimeout(delay)
+
   }, [name])
 
   return(
     <>
-      {pokemonContainer()}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='name'>Pokemon ID or name: </label>
-        <input type='text' name='name' id='name' value={name} onChange={(e) => setName(e.target.value)}></input>
-      </form>
+      <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
+      <div>{isLoading && <p>Cargando</p>}{error && !pokemon && <p>{error}</p>}</div>
+      {pokemon && 
+      <div className='content'>
+        <div className={`contentPokemon ${pokemon.types[0].type.name}`}>
+          <h2>{pokemon.id}. {pokemon.name}</h2>
+          <div className='pokemon'>
+            <img src={pokemon.sprites?.other['official-artwork']?.front_default} alt={pokemon.name}/>
+          </div>
+          
+        </div>
+      </div>
+      }
     </>
   )
 }
